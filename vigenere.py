9b814c84fdc5f5dbab160a723102e53631071b4a
Josh <joshuamcdowell97@gmail.com>
2019-03-28 16:58:34 -0500

    up to date vigenere

--- vigenere.py
+++ vigenere.py
@@ -4,23 +4,21 @@
 #
 
 import sys
+from string import whitespace
 
 
 
 def scan():
     typeMod = sys.argv[1]
     key = sys.argv[2]
-
+    key = key.translate(None, whitespace)
     if(typeMod == "-e"):
-        # ty = 0
         encode(key)
 
     elif (typeMod == "-d"):
-        # ty = 1
         decode(key)
 
     else:
-        # ty = -1
         print("Please enter -e or -d for encode or decode")
         exit()
 
@@ -35,24 +33,109 @@ def encode(k):
 
         if(phrase == ""):
             exit()
-        k = k.replace('\n', '')
-        k = k.replace('\t', '')
-        k = k.replace('\n', '')
-
-        # for i in range(0, len(phrase)):
-            # if(
+        elif '\n' in phrase:
+            phrase = phrase[:len(phrase)-1]
         
         for i in range(0, len(phrase)):
-            keyIndex = i % len(k)
-            currKey = k[keyIndex]
-            currPh = phrase[keyIndex]
-            # encString += chr((int(currKey) + int(currPh)) % 64)
+            isSymb = False
+            isCapital = False
 
-            \
+
+            if 'a' <= phrase[i] <= 'z':
+                phInt = ord(phrase[i]) - 97
+            elif 'A' <= phrase[i] <= 'Z':
+                phInt = ord(phrase[i]) - 65
+                isCapital = True
+            elif phrase[i] == ' ':
+                encString += ' '
+                continue
+            else:
+                phInt = ord(phrase[i])
+                isSymb = True
+
+            if 'a' <= k[keyIter] <= 'z':
+                keyInt = ord(k[keyIter]) - 97
+            elif 'A' <= k[keyIter] <= 'Z':
+                keyInt = ord(k[keyIter]) - 65
+            else:
+                print("Key contains non-alphabetical character.")
+                exit()
+
+
+            if keyIter >= len(k) - 1:
+                keyIter = 0
+            else:
+                keyIter += 1
+
+            if isSymb:
+                encString += chr(phInt)
+                keyIter -= 1
+                if keyInt < 0:
+                    keyInt = len(k) - 1
+
+            elif isCapital:
+                encString += chr(((phInt + keyInt) % 26) + 65)
+            else:
+                encString += chr(((phInt + keyInt) % 26) + 97)
+
+
+        print(encString)
 
 def decode(k):
     while True:
+        phrase = sys.stdin.readline()
+        decString = ""
+        keyIter = 0
 
+        if(phrase == ""):
+            exit()
+        elif '\n' in phrase:
+            phrase = phrase[:len(phrase)-1]
+        
+        for i in range(0, len(phrase)):
+            isSymb = False
+            isCapital = False
+
+
+            if 'a' <= phrase[i] <= 'z':
+                phInt = ord(phrase[i]) - 97
+            elif 'A' <= phrase[i] <= 'Z':
+                phInt = ord(phrase[i]) - 65
+                isCapital = True
+            elif phrase[i] == ' ':
+                decString += ' '
+                continue
+            else:
+                phInt = ord(phrase[i])
+                isSymb = True
+
+            if 'a' <= k[keyIter] <= 'z':
+                keyInt = ord(k[keyIter]) - 97
+            elif 'A' <= k[keyIter] <= 'Z':
+                keyInt = ord(k[keyIter]) - 65
+            else:
+                print("Key contains non-alphabetical character.")
+                exit()
+
+
+            if keyIter >= len(k) - 1:
+                keyIter = 0
+            else:
+                keyIter += 1
+
+            if isSymb:
+                decString += chr(phInt)
+                keyIter -= 1
+                if keyInt < 0:
+                    keyInt = len(k) - 1
+
+            elif isCapital:
+                decString += chr(((26 + phInt - keyInt) % 26) + 65)
+            else:
+                decString += chr(((26 + phInt - keyInt) % 26) + 97)
+
+
+        print(decString)
 
 
 if(len(sys.argv) >= 3):
